54d4d4e2fe4f9e2df45aa532a828f922b9607de0
Sahaj <Sahaj.Arora@fenergo.com>
2022-03-26 19:35:12 +0530

    requirements.txt

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 import pandas as pd 
 import streamlit as st 
 from st_aggrid import AgGrid
+from st_aggrid.grid_options_builder import GridOptionsBuilder
 
 st.set_page_config(page_title="IB File", layout="wide") 
 st.title("IB File")
 
 shows = pd.read_csv("./usa.txt",on_bad_lines='warn',sep="|")  
+gb = GridOptionsBuilder.from_dataframe(shows)
 
-AgGrid(shows)
+# add this
+gb.configure_pagination()
+gb.configure_side_bar()
+gb.configure_default_column(groupable=True, value=True, enableRowGroup=True, aggFunc="sum", editable=True)
+gridOptions = gb.build()
+
+AgGrid(shows, gridOptions=gridOptions, enable_enterprise_modules=True)
